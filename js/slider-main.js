9c2301e4ddb8b7f73be14f57528381cad239cb02
ZorAnderius <gabriel.zorander@gmail.com>
2023-02-22 21:51:59 +0000

    add slider

---
+++ js/slider-main.js
@@ -0,0 +1,12 @@
+function prev() {
+  document.getElementById("slider-container").scrollLeft -= 270;
+}
+
+function next() {
+  document.getElementById("slider-container").scrollLeft += 270;
+}
+
+$(".slide img").on("click", function () {
+  $(this).toggleClass("zoomed");
+  $(".overlay").toggleClass("active");
+});
